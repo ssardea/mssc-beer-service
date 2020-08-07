ab5fe865a1df539e9cefab6f2ecad53559227a61
rooootxx <ssardea@gmail.com>
2020-08-07 23:37:00 +0800

    Initial test issue Closes #1

--- src/main/java/sam/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/sam/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -1,13 +1,20 @@
 package sam.springframework.msscbeerservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class MsscBeerServiceApplication {
+public class MsscBeerServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MsscBeerServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Test Close Issue 1");
+    }
 }
