f6aba16538c4d8adedfc85a480d910d3187eb3db
rooootxx <ssardea@gmail.com>
2020-08-08 00:12:21 +0800

    Create Beer Controller Closes #3

---
+++ src/main/java/sam/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,30 @@
+package sam.springframework.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import sam.springframework.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    private ResponseEntity<BeerDto> getBeerByID(@PathVariable("beerID") UUID beerID){
+
+        return new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    private ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    private ResponseEntity updateBeer(@PathVariable("beerId") UUID beerID, @RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
