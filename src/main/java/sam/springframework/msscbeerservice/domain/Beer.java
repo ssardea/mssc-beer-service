14b78f9a92c11b3c95206758dd84f2432239208e
rooootxx <ssardea@gmail.com>
2020-08-10 14:08:13 +0800

    Create JPA for Beer Closes #5

--- src/main/java/sam/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/sam/springframework/msscbeerservice/domain/Beer.java
@@ -20,6 +20,7 @@ import java.util.UUID;
 @Builder
 @Entity
 public class Beer {
+
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name= "UUID", strategy = "org.hibernate.UUIDGenerator")
